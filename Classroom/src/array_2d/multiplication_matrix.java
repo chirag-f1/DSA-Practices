3cc23b6156b4d00288b3d78710e8362cf11bc571
chirag-f1 <chiraggoyal.vnm@gmail.com>
2026-01-29 23:30:06 +0530

    multiplication matrix

---
+++ Classroom/src/array_2d/multiplication_matrix.java
@@ -0,0 +1,55 @@
+package Classroom.src.array_2d;
+import java.util.Scanner;
+
+public class multiplication_matrix{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r1=sc.nextInt();
+        int c1=sc.nextInt();
+        int a[][]= new int[r1][c1];
+        System.out.println("enter the first matrix");
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                a[i][j]=sc.nextInt();
+            }
+        }
+        int r2=sc.nextInt();
+        int c2 = sc.nextInt();
+        int b[][]= new int[r2][c2];
+
+        int mul[][]=new int[r1][c2];
+        System.out.println("enter the second matrix");
+        for (int i = 0; i < r2; i++) {
+            for (int j = 0; j < c2; j++) {
+                b[i][j]=sc.nextInt();
+            }
+            if(r2!=c1){
+                System.out.println("matrix doesn't exist bcoz of size bonding :");
+break;
+            }
+        }
+
+
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                for (int k = 0; k < c1; k++) {
+                    mul[i][j]+= a[i][k]*b[k][j];
+
+
+                }
+
+            }
+
+        }
+
+
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                System.out.print(mul[i][j]+ " ");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
